fc8cadd10f31cfaa493f8db308878ed9c49b67b5
marcosdanieldasilva <marcosdasilva@5a.tec.br>
2026-03-17 16:57:56 -0300

    add docs

---
+++ docs/make.jl
@@ -0,0 +1,23 @@
+using ForestEcology
+using Documenter
+
+DocMeta.setdocmeta!(ForestEcology, :DocTestSetup, :(using ForestEcology); recursive=true)
+
+makedocs(;
+    modules=[ForestEcology],
+    authors="Marcos Daniel da Silva <marcosdasilva@5a.tec.br> and contributors",
+    sitename="ForestEcology.jl",
+    format=Documenter.HTML(;
+        canonical="https://JuliaForests.github.io/ForestEcology.jl",
+        edit_link="master",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/JuliaForests/ForestEcology.jl",
+    devbranch="master",
+)
---
+++ docs/src/index.md
@@ -0,0 +1,14 @@
+```@meta
+CurrentModule = ForestEcology
+```
+
+# ForestEcology
+
+Documentation for [ForestEcology](https://github.com/JuliaForests/ForestEcology.jl).
+
+```@index
+```
+
+```@autodocs
+Modules = [ForestEcology]
+```
